cef0a2e3509c4d806f0c871c8430b8840ba75b96
Charles Garwood <cgarwood@gmail.com>
2019-11-06 13:58:17 -0500

    initial commit

---
+++ openzwavemqtt/const.py
@@ -0,0 +1,15 @@
+EVENT_PLACEHOLDER = "missing"
+EVENT_INSTANCE_ADDED = "instance_added"
+EVENT_INSTANCE_CHANGED = "instance_changed"
+EVENT_INSTANCE_REMOVED = "instance_removed"
+
+EVENT_INSTANCE_STATISTICS_CHANGED = "instance_statistics_changed"
+EVENT_INSTANCE_STATUS_CHANGED = "instance_status_changed"
+
+EVENT_NODE_ADDED = "node_added"
+EVENT_NODE_CHANGED = "node_changed"
+EVENT_NODE_REMOVED = "node_removed"
+
+EVENT_VALUE_ADDED = "value_added"
+EVENT_VALUE_CHANGED = "value_changed"
+EVENT_VALUE_REMOVED = "value_removed"
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="python-openzwave-mqtt",
+    version="0.0.1",
+    url="https://github.com/cgarwood/python-openzwave-mqtt",
+    download_url="https://github.com/cgarwood/python-openzwave-mqtt",
+    author="Charles Garwood",
+    author_email="cgarwood@gmail.com",
+    packages=["openzwavemqtt"],
+    include_package_data=True,
+    zip_safe=False,
+    test_suite="tests",
+)
