0c9ed8defed3b5d3ba5ee00250ece7998aa24bfa
Charles Garwood <cgarwood@gmail.com>
2019-11-25 11:21:41 -0500

    add item_id to test (#27)

--- test/models/test_node_child_base.py
+++ test/models/test_node_child_base.py
@@ -10,13 +10,13 @@ class MockDescendant(OZWNodeChildBase):
 
 
 def test_node():
-    node = OZWNode(None, None, "mock-node-id")
+    node = OZWNode(None, None, "mock-node-id", 1)
 
-    child = MockDescendant(None, node, "mock-child-id")
+    child = MockDescendant(None, node, "mock-child-id", 12)
     assert child.node is node
 
-    grandchild = MockDescendant(None, child, "mock-grandchild-id")
+    grandchild = MockDescendant(None, child, "mock-grandchild-id", 123)
     assert grandchild.node is node
 
     with pytest.raises(RuntimeError):
-        MockDescendant(None, None, "").node
+        MockDescendant(None, None, "", "").node
