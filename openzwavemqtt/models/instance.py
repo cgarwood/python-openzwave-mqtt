e9f6d561b161f82156fb32c233240451c020bcb0
Marcel van der Veldt <m.vanderveldt@outlook.com>
2020-01-19 22:16:31 +0100

    Instance commands
    Shortcut methods to some common used (global) controller commands

--- openzwavemqtt/models/instance.py
+++ openzwavemqtt/models/instance.py
@@ -50,12 +50,38 @@ class OZWInstance(ZWaveBase):
         }
 
     def send_command(self, command: str, payload: str = ""):
+        """Send command to the OZW instance"""
         topic_prefix = self.options.topic_prefix
         full_topic = f"{topic_prefix}{self.id}/command/{command}/"
-        self.options.send_command(full_topic, payload)
+        self.options.send_message(full_topic, payload)
 
-    def add_node(self, secure=False):
+    # Shortcut methods to some common used (global) controller commands
+    # https://github.com/OpenZWave/qt-openzwave/blob/master/docs/MQTT.md#mqtt-commands
+
+    def add_node(self, secure: bool = False):
+        """Enter inclusion mode on the controller."""
         self.send_command("addnode", {"secure": secure})
 
+    def remove_node(self):
+        """Enter exclusion mode on the controller."""
+        self.send_command("removenode")
+
+    def refresh_node(self, node_id: int):
+        """Force OZW to re-interview a device."""
+        self.send_command("refreshnodeinfo", {"node": node_id})
+
+    def remove_failed_node(self, node_id: int):
+        """Remove a failed node from the controller."""
+        self.send_command("removefailednode", {"node": node_id})
+
+    def replace_failed_node(self, node_id: int):
+        """Replace a failed node from the controller with a new device."""
+        self.send_command("replacefailednode", {"node": node_id})
+
+    def heal_node(self, node_id: int):
+        """Asks a Node to recalculate its neighbors and routes to other devices.​"""
+        self.send_command("healnetworknode", {"node": node_id})
+
     def cancel_controller_command(self):
+        """Cancels in Controller Commands that are in progress."""
         self.send_command("cancelcontrollercommand")
