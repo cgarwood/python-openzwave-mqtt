85049e644c847a566585773f66df6e08fc11ea28
Paulus Schoutsen <balloob@gmail.com>
2020-02-14 08:47:15 -0800

    Update instance/node statistics (#48)
    * Update instance/node statistics
    
    * Update test

--- openzwavemqtt/models/node_statistics.py
+++ openzwavemqtt/models/node_statistics.py
@@ -1,4 +1,6 @@
 """Model for Node Statistics."""
+from typing import List
+
 from ..base import ZWaveBase
 from ..const import EVENT_NODE_ADDED, EVENT_NODE_STATISTICS_CHANGED, EVENT_NODE_REMOVED
 
@@ -10,112 +12,172 @@ class OZWNodeStatistics(ZWaveBase):
     EVENT_REMOVED = EVENT_NODE_REMOVED
 
     @property
-    def ack_channel(self):
-        """Return ackChannel."""
-        return self.data.get("ackChannel")
+    def send_count(self) -> int:
+        """Return sendCount."""
+        return self.data.get("sendCount")
 
     @property
-    def average_request_rtt(self):
-        """Return averageRequestRTT."""
-        return self.data.get("averageRequestRTT")
+    def sent_failed(self) -> int:
+        """Return sentFailed."""
+        return self.data.get("sentFailed")
 
     @property
-    def average_response_rtt(self):
-        """Return averageResponseRTT."""
-        return self.data.get("averageResponseRTT")
-
-    @property
-    def extended_tx_supported(self):
-        """Return extendedTXSupported."""
-        return self.data.get("extendedTXSupported")
-
-    @property
-    def hops(self):
-        """Return hops."""
-        return self.data.get("hops")
-
-    @property
-    def last_failed_link_from(self):
-        """Return lastFailedLinkFrom."""
-        return self.data.get("lastFailedLinkFrom")
-
-    @property
-    def last_failed_link_to(self):
-        """Return lastFailedLinkTo."""
-        return self.data.get("lastFailedLinkTo")
-
-    @property
-    def last_received_time_stamp(self):
-        """Return lastRecievedTimeStamp."""
-        return self.data.get("lastRecievedTimeStamp")
-
-    @property
-    def last_request_rtt(self):
-        """Return lastRequestRTT."""
-        return self.data.get("lastRequestRTT")
-
-    @property
-    def last_response_rtt(self):
-        """Return lastResponseRTT."""
-        return self.data.get("lastResponseRTT")
-
-    @property
-    def last_sent_time_stamp(self):
-        """Return lastSentTimeStamp."""
-        return self.data.get("lastSentTimeStamp")
-
-    @property
-    def last_tx_channel(self):
-        """Return lastTXChannel."""
-        return self.data.get("lastTXChannel")
-
-    @property
-    def quality(self):
-        """Return quality."""
-        return self.data.get("quality")
-
-    @property
-    def received_dup_packets(self):
-        """Return receivedDupPackets."""
-        return self.data.get("receivedDupPackets")
-
-    @property
-    def received_packets(self):
-        """Return recievedPackets."""
-        return self.data.get("recievedPackets")
-
-    @property
-    def received_unsolicited(self):
-        """Return receivedUnsolicited."""
-        return self.data.get("receivedUnsolicited")
-
-    @property
-    def retries(self):
+    def retries(self) -> int:
         """Return retries."""
         return self.data.get("retries")
 
     @property
-    def route_scheme(self):
+    def received_packets(self) -> int:
+        """Return receivedPackets."""
+        return self.data.get("receivedPackets")
+
+    @property
+    def received_dup_packets(self) -> int:
+        """Return receivedDupPackets."""
+        return self.data.get("receivedDupPackets")
+
+    @property
+    def received_unsolicited(self) -> int:
+        """Return receivedUnsolicited."""
+        return self.data.get("receivedUnsolicited")
+
+    @property
+    def last_sent_time_stamp(self) -> int:
+        """Return lastSentTimeStamp."""
+        return self.data.get("lastSentTimeStamp")
+
+    @property
+    def last_received_time_stamp(self) -> int:
+        """Return lastReceivedTimeStamp."""
+        return self.data.get("lastReceivedTimeStamp")
+
+    @property
+    def last_request_rtt(self) -> int:
+        """Return lastRequestRTT."""
+        return self.data.get("lastRequestRTT")
+
+    @property
+    def average_request_rtt(self) -> int:
+        """Return averageRequestRTT."""
+        return self.data.get("averageRequestRTT")
+
+    @property
+    def last_response_rtt(self) -> int:
+        """Return lastResponseRTT."""
+        return self.data.get("lastResponseRTT")
+
+    @property
+    def average_response_rtt(self) -> int:
+        """Return averageResponseRTT."""
+        return self.data.get("averageResponseRTT")
+
+    @property
+    def quality(self) -> int:
+        """Return quality."""
+        return self.data.get("quality")
+
+    @property
+    def extended_tx_supported(self) -> bool:
+        """Return extendedTXSupported."""
+        return self.data.get("extendedTXSupported")
+
+    @property
+    def tx_time(self) -> int:
+        """Return txTime."""
+        return self.data.get("txTime")
+
+    @property
+    def hops(self) -> int:
+        """Return hops."""
+        return self.data.get("hops")
+
+    @property
+    def rssi1(self) -> str:
+        """Return rssi_1."""
+        return self.data.get("rssi_1")
+
+    @property
+    def rssi2(self) -> str:
+        """Return rssi_2."""
+        return self.data.get("rssi_2")
+
+    @property
+    def rssi3(self) -> str:
+        """Return rssi_3."""
+        return self.data.get("rssi_3")
+
+    @property
+    def rssi4(self) -> str:
+        """Return rssi_4."""
+        return self.data.get("rssi_4")
+
+    @property
+    def rssi5(self) -> str:
+        """Return rssi_5."""
+        return self.data.get("rssi_5")
+
+    @property
+    def route1(self) -> int:
+        """Return route_1."""
+        return self.data.get("route_1")
+
+    @property
+    def route2(self) -> int:
+        """Return route_2."""
+        return self.data.get("route_2")
+
+    @property
+    def route3(self) -> int:
+        """Return route_3."""
+        return self.data.get("route_3")
+
+    @property
+    def route4(self) -> int:
+        """Return route_4."""
+        return self.data.get("route_4")
+
+    @property
+    def ack_channel(self) -> int:
+        """Return ackChannel."""
+        return self.data.get("ackChannel")
+
+    @property
+    def last_tx_channel(self) -> int:
+        """Return lastTXChannel."""
+        return self.data.get("lastTXChannel")
+
+    @property
+    def route_scheme(self) -> str:
         """Return routeScheme."""
         return self.data.get("routeScheme")
 
     @property
-    def route_speed(self):
-        """Return routeSpeed."""
-        return self.data.get("routeSpeed")
-
-    @property
-    def route_tries(self):
-        """Return routeTries."""
-        return self.data.get("routeTries")
-
-    @property
-    def route_used(self):
+    def route_used(self) -> str:
         """Return routeUsed."""
         return self.data.get("routeUsed")
 
     @property
-    def routes(self):
+    def route_speed(self) -> str:
+        """Return routeSpeed."""
+        return self.data.get("routeSpeed")
+
+    @property
+    def route_tries(self) -> int:
+        """Return routeTries."""
+        return self.data.get("routeTries")
+
+    @property
+    def last_failed_link_from(self) -> int:
+        """Return lastFailedLinkFrom."""
+        return self.data.get("lastFailedLinkFrom")
+
+    @property
+    def last_failed_link_to(self) -> int:
+        """Return lastFailedLinkTo."""
+        return self.data.get("lastFailedLinkTo")
+
+    @property
+    def routes(self) -> List[int]:
         """Return routes."""
         return [
             self.data.get("route_1"),
@@ -125,7 +187,7 @@ class OZWNodeStatistics(ZWaveBase):
         ]
 
     @property
-    def rssi(self):
+    def rssi(self) -> List[int]:
         """Return rssi."""
         return [
             self.data.get("rssi_1"),
@@ -134,18 +196,3 @@ class OZWNodeStatistics(ZWaveBase):
             self.data.get("rssi_4"),
             self.data.get("rssi_5"),
         ]
-
-    @property
-    def send_count(self):
-        """Return sendCount."""
-        return self.data.get("sendCount")
-
-    @property
-    def sent_failed(self):
-        """Return sentFailed."""
-        return self.data.get("sentFailed")
-
-    @property
-    def tx_time(self):
-        """Return txTime."""
-        return self.data.get("txTime")
