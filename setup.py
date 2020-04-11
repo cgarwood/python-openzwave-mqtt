d9d99c7d63e0b6a682ca6cb5021ac0ca380a1d8b
Martin Hjelmare <marhje52@gmail.com>
2020-04-12 01:41:57 +0200

    Clean setup.py and add classifiers (#62)
    * Remove deprecated test suite parameter
    
    * Use pathlib to read readme
    
    * Set Python 3.6 as min version
    
    * Fix test package exclusion
    
    * Add classifiers

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 """Setup module for python-openzwave-mqtt."""
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
+PROJECT_DIR = Path(__file__).parent.resolve()
+README_FILE = PROJECT_DIR / "README.md"
 VERSION = "0.0.8"
-
-
-def readme():
-    """Print long description."""
-    with open("README.md") as f:
-        return f.read()
 
 
 setup(
@@ -18,10 +16,20 @@ setup(
     author="Charles Garwood",
     author_email="cgarwood@gmail.com",
     description="Converts MQTT messages from qt-openzwave into Python objects and events",
-    long_description=readme(),
+    long_description=README_FILE.read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
-    packages=find_packages(exclude=["*.tests"]),
+    packages=find_packages(exclude=["test.*", "test"]),
+    python_requires=">=3.6",
     include_package_data=True,
     zip_safe=False,
-    test_suite="tests",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Home Automation",
+    ],
 )
