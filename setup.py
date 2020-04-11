445cf4efe0b02796f1cc39ad3196f492afabd1ee
Martin Hjelmare <marhje52@gmail.com>
2020-04-12 01:51:19 +0200

    Add mypy tox env

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=["test.*", "test"]),
     python_requires=">=3.6",
+    install_requires=["typing-extensions"],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
