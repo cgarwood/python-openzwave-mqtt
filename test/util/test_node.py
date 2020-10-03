34f5376dd97650828dff21f7eeb57b1af455ecb5
raman325 <7243222+raman325@users.noreply.github.com>
2020-10-03 11:58:00 -0400

    use set_config_parameter in tests

--- test/util/test_node.py
+++ test/util/test_node.py
@@ -1,94 +1,101 @@
 """Tests for node util submodule."""
-from unittest.mock import Mock
+from openzwavemqtt.models.node import OZWNode
+from unittest.mock import Mock, patch
 
 import pytest
 
 from openzwavemqtt.const import ATTR_LABEL, ATTR_POSITION, ATTR_VALUE, ValueType
 from openzwavemqtt.exceptions import InvalidValueError, NotFoundError, WrongTypeError
 from openzwavemqtt.models.value import OZWValue
-from openzwavemqtt.util.node import (
-    _set_bitset_config_parameter,
-    _set_bool_config_parameter,
-    _set_int_config_parameter,
-    _set_list_config_parameter,
-)
+from openzwavemqtt.util.node import set_config_parameter
 
 
-def test_set_bool_config_parameter():
+def test_set_bool_config_parameter(options):
     """Test setting a ValueType.BOOL config parameter."""
     mock_value = Mock(spec=OZWValue)
     mock_value.type = ValueType.BOOL
 
-    _set_bool_config_parameter(mock_value, True)
-    _set_bool_config_parameter(mock_value, False)
-    _set_bool_config_parameter(mock_value, "True")
-    _set_bool_config_parameter(mock_value, "False")
+    node = OZWNode(options, None, "test", None)
 
-    with pytest.raises(WrongTypeError):
-        _set_bool_config_parameter(mock_value, "test")
+    with patch("openzwavemqtt.util.node.OZWNode.get_value", return_value=mock_value):
+        assert set_config_parameter(node, 1, True)
+        assert not set_config_parameter(node, 1, False)
+        assert set_config_parameter(node, 1, "True")
+        assert not set_config_parameter(node, 1, "False")
 
-    with pytest.raises(WrongTypeError):
-        _set_bool_config_parameter(mock_value, 95)
+        with pytest.raises(WrongTypeError):
+            set_config_parameter(node, 1, "test")
+
+        with pytest.raises(WrongTypeError):
+            set_config_parameter(node, 1, 95)
 
 
-def test_set_list_config_parameter():
+def test_set_list_config_parameter(options):
     """Test setting a ValueType.LIST config parameter."""
     mock_value = Mock(spec=OZWValue)
     mock_value.type = ValueType.LIST
     mock_value.value = {"List": [{"Label": "test", "Value": 0}]}
 
-    _set_list_config_parameter(mock_value, "0")
-    _set_list_config_parameter(mock_value, 0)
-    _set_list_config_parameter(mock_value, "test")
+    node = OZWNode(options, None, "test", None)
 
-    with pytest.raises(NotFoundError):
-        _set_list_config_parameter(mock_value, 1)
+    with patch("openzwavemqtt.util.node.OZWNode.get_value", return_value=mock_value):
+        assert set_config_parameter(node, 1, "0") == 0
+        assert set_config_parameter(node, 1, 0) == 0
+        assert set_config_parameter(node, 1, "test") == 0
 
-    with pytest.raises(WrongTypeError):
-        _set_list_config_parameter(mock_value, ["test"])
+        with pytest.raises(NotFoundError):
+            set_config_parameter(node, 1, 1)
+
+        with pytest.raises(WrongTypeError):
+            set_config_parameter(node, 1, ["test"])
 
 
-def test_set_bitset_config_parameter():
+def test_set_bitset_config_parameter(options):
     """Test setting a ValueType.BITSET config parameter."""
     mock_value = Mock(spec=OZWValue)
-    mock_value.type = ValueType.BOOL
+    mock_value.type = ValueType.BITSET
     mock_value.value = [{"Position": 1, "Label": "test", "Value": False}]
 
-    with pytest.raises(WrongTypeError):
-        _set_bitset_config_parameter(
-            mock_value, [{ATTR_POSITION: 1, ATTR_LABEL: "test", ATTR_VALUE: True}]
-        )
+    node = OZWNode(options, None, "test", None)
 
-    with pytest.raises(WrongTypeError):
-        _set_bitset_config_parameter(mock_value, [{ATTR_VALUE: True}])
+    with patch("openzwavemqtt.util.node.OZWNode.get_value", return_value=mock_value):
+        with pytest.raises(WrongTypeError):
+            set_config_parameter(
+                node, 1, [{ATTR_POSITION: 1, ATTR_LABEL: "test", ATTR_VALUE: True}]
+            )
 
-    with pytest.raises(WrongTypeError):
-        _set_bitset_config_parameter(
-            mock_value, [{ATTR_POSITION: "test", ATTR_VALUE: True}]
-        )
+        with pytest.raises(WrongTypeError):
+            set_config_parameter(node, 1, [{ATTR_VALUE: True}])
 
-    with pytest.raises(WrongTypeError):
-        _set_bitset_config_parameter(mock_value, [{ATTR_LABEL: 1, ATTR_VALUE: True}])
+        with pytest.raises(WrongTypeError):
+            set_config_parameter(node, 1, [{ATTR_POSITION: "test", ATTR_VALUE: True}])
 
-    with pytest.raises(WrongTypeError):
-        _set_bitset_config_parameter(mock_value, [{ATTR_POSITION: 1}])
+        with pytest.raises(WrongTypeError):
+            set_config_parameter(node, 1, [{ATTR_LABEL: 1, ATTR_VALUE: True}])
 
-    with pytest.raises(WrongTypeError):
-        _set_bitset_config_parameter(mock_value, [{ATTR_POSITION: 1, ATTR_VALUE: 1}])
+        with pytest.raises(WrongTypeError):
+            set_config_parameter(node, 1, [{ATTR_POSITION: 1}])
 
-    with pytest.raises(NotFoundError):
-        _set_bitset_config_parameter(mock_value, [{ATTR_POSITION: 2, ATTR_VALUE: True}])
+        with pytest.raises(WrongTypeError):
+            set_config_parameter(node, 1, [{ATTR_POSITION: 1, ATTR_VALUE: 1}])
 
-    with pytest.raises(NotFoundError):
-        _set_bitset_config_parameter(
-            mock_value, [{ATTR_LABEL: "test not found", ATTR_VALUE: True}]
-        )
+        with pytest.raises(NotFoundError):
+            set_config_parameter(node, 1, [{ATTR_POSITION: 2, ATTR_VALUE: True}])
 
-    _set_bitset_config_parameter(mock_value, [{ATTR_LABEL: "test", ATTR_VALUE: True}])
-    _set_bitset_config_parameter(mock_value, [{ATTR_POSITION: 1, ATTR_VALUE: True}])
+        with pytest.raises(NotFoundError):
+            set_config_parameter(
+                node, 1, [{ATTR_LABEL: "test not found", ATTR_VALUE: True}]
+            )
+
+        assert set_config_parameter(
+            node, 1, [{ATTR_LABEL: "test", ATTR_VALUE: True}]
+        ) == [{ATTR_LABEL: "test", ATTR_VALUE: True}]
+        assert set_config_parameter(
+            node, 1, [{ATTR_POSITION: 1, ATTR_VALUE: True}]
+        ) == [{ATTR_POSITION: 1, ATTR_VALUE: True}]
 
 
-def test_set_int_config_parameter():
+def test_set_int_config_parameter(options):
     """Test setting a ValueType.INT config parameter."""
     mock_value = Mock(spec=OZWValue)
     mock_value.type = ValueType.INT
@@ -96,13 +103,17 @@ def test_set_int_config_parameter():
     mock_value.min = 0
     mock_value.max = 10
 
-    with pytest.raises(WrongTypeError):
-        _set_int_config_parameter(mock_value, "test")
+    node = OZWNode(options, None, "test", None)
 
-    with pytest.raises(InvalidValueError):
-        _set_int_config_parameter(mock_value, -1)
+    with patch("openzwavemqtt.util.node.OZWNode.get_value", return_value=mock_value):
+        with pytest.raises(WrongTypeError):
+            set_config_parameter(node, 1, "test")
 
-    with pytest.raises(InvalidValueError):
-        _set_int_config_parameter(mock_value, 11)
+        with pytest.raises(InvalidValueError):
+            set_config_parameter(node, 1, -1)
 
-    _set_int_config_parameter(mock_value, 1)
+        with pytest.raises(InvalidValueError):
+            set_config_parameter(node, 1, 11)
+
+        assert set_config_parameter(node, 1, 1) == 1
+        assert set_config_parameter(node, 1, "1") == 1
